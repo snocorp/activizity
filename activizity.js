d501e69d922459028cf903356a46aa8b1bd58950
Dave Sewell <snocorp@gmail.com>
2015-06-13 15:55:24 -0400

    Header Building

--- activizity.js
+++ activizity.js
@@ -1,47 +1,5 @@
 /*! activizity v0.0.0 - MIT license */
 
-var data = {
-  '2015-06-12': {
-    '10001': {
-      'name': 'Dave Sewell',
-      'entries': {
-        'overhead': {
-          'minutes': 180,
-          'comments': ['convention prep']
-        },
-        'fsapp': {
-          'minutes': 300,
-          'comments': ['investigation into barcode issue, notifications']
-        }
-      }
-    },
-    '10002': {
-      'name': 'Jonathan Clarkin',
-      'entries': {
-        'vacation': {
-          'minutes': 480
-        }
-      }
-    }
-  },
-  '2015-06-11': {
-    '10002': {
-      'name': 'Jonathan Clarkin',
-      'entries': {
-        'vacation': {
-          'minutes': 480
-        }
-      }
-    }
-  },
-  '2015-06-10': {
-  },
-  '2015-06-09': {
-  },
-  '2015-06-08': {
-  }
-}
-
 ;(function (global) {
 'use strict';
 function moduleDefinition(_, d3, moment) {
@@ -58,60 +16,82 @@ function activizity(options) {
   options = options || {};
   _.defaults(options, {
     containerSelector: '#activizity',
+    data: function(callback) { callback(null,{}); },
     dateFormat: 'YYYY-MM-DD',
     displayDateFormat: 'D MMM YYYY',
     displayMonthFormat: 'MMM YYYY',
+    locale: 'en-ca'
   });
 
-  var dates = _.keys(data).sort();
-  if (dates.length === 0) {
-    return;
-  }
+  moment.locale(options.locale);
 
-  var currentMonth = moment(dates[0], options.dateFormat);
-  var projects =
-    // make the list unique
-    _.uniq(
-      // flatten the arrays of project keys
-      _.flatten(
-        // get the project keys from the entries
-        _.map(
-          //pluck the entries from the user info
-          _.pluck(
-            //flatten the arrays of user info
-            _.flatten(
-              _.map(
-                // get the values from the days: the userId->userInfo map
-                _.values(data),
-                function(users) {
-                  //get the values from the users: the userInfo
-                  return _.values(users);
-                }
-              )
-            ), 'entries'
-          ),
-          function(entries) { return _.keys(entries); }
+  options.data(function(error, data) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    var dates = _.keys(data).sort();
+    if (dates.length === 0) {
+      console.log('No dates provided');
+      return;
+    }
+
+    var currentMonth = moment(dates[0], options.dateFormat);
+    var projects =
+      // make the list unique
+      _.uniq(
+        // flatten the arrays of project keys
+        _.flatten(
+          // get the project keys from the entries
+          _.map(
+            //pluck the entries from the user info
+            _.pluck(
+              //flatten the arrays of user info
+              _.flatten(
+                _.map(
+                  // get the values from the days: the userId->userInfo map
+                  _.values(data),
+                  function(users) {
+                    //get the values from the users: the userInfo
+                    return _.values(users);
+                  }
+                )
+              ), 'entries'
+            ),
+            function(entries) { return _.keys(entries); }
+          )
         )
-      )
-    );
+      );
 
-  console.log(projects);
+    var weekdaysShort = moment.weekdaysShort();
+    weekdaysShort.unshift('');
 
-  var header = d3.select(options.containerSelector)
-    .append('div')
-    .attr('class', 'activity-calendar')
-    .append('div')
-    .attr('class', 'activity-header');
-  header.append('h3')
-    .attr('class', 'current-month')
-    .text(currentMonth.format(options.displayMonthFormat));
-  var projectList = header.append('dl')
-    .attr('class', 'project-key');
-  projectList.selectAll('dd')
-    .data(projects)
-    .enter().append('dd')
-    .attr('class', function(p) { return p; })
-    .text(function(p) { return '#' + p; });
+    var calendar = d3.select(options.containerSelector)
+      .append('div')
+      .attr('class', 'activity-calendar');
+    var header = calendar.append('div')
+      .attr('class', 'activity-header');
+    header.append('h3')
+      .attr('class', 'current-month')
+      .text(currentMonth.format(options.displayMonthFormat));
+    var projectList = header.append('dl')
+      .attr('class', 'project-key');
+    projectList.selectAll('dd')
+      .data(projects)
+      .enter().append('dd')
+      .attr('class', function(p) { return p; })
+      .text(function(p) { return '#' + p; });
+    var daysList = header.append('ul')
+      .attr('class', 'days');
+    daysList.selectAll('li')
+      .data(weekdaysShort)
+      .enter().append('li')
+      .text(function(w) { return w; });
+
+    calendar.append('div')
+      .attr('class', 'activity-intervals');
+  });
 }
 
 /**
--- demo/js/main.js
+++ demo/js/main.js
@@ -3,12 +3,56 @@ require.config({
     activizity: '../../activizity',
     d3: "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3",
     lodash: "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.9.3/lodash",
-    moment: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment"
+    moment: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment-with-locales"
   }
 });
 
 require(['activizity'], function(activizity) {
   'use strict';
 
-  var a = activizity();
+  activizity({
+    data: function(callback) {
+      callback(null,{
+        '2015-06-12': {
+          '10001': {
+            'name': 'Dave Sewell',
+            'entries': {
+              'overhead': {
+                'minutes': 180,
+                'comments': ['convention prep']
+              },
+              'fsapp': {
+                'minutes': 300,
+                'comments': ['investigation into barcode issue, notifications']
+              }
+            }
+          },
+          '10002': {
+            'name': 'Jonathan Clarkin',
+            'entries': {
+              'vacation': {
+                'minutes': 480
+              }
+            }
+          }
+        },
+        '2015-06-11': {
+          '10002': {
+            'name': 'Jonathan Clarkin',
+            'entries': {
+              'vacation': {
+                'minutes': 480
+              }
+            }
+          }
+        },
+        '2015-06-10': {
+        },
+        '2015-06-09': {
+        },
+        '2015-06-08': {
+        }
+      });
+    }
+  });
 });
